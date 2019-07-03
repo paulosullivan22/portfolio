b6c3be9ff7618fe16d3b420a839d98354d692293
paul <paulosullivan22@outlook.com>
2019-07-03 10:26:17 +0200

    testing background flow

--- src/background.js
+++ src/background.js
@@ -1,13 +1,19 @@
 class Background {
     constructor() {
-      this.x1 = WIDTH;      // Sets initial position of two images for each level of foreground
-      this.x11 = 0;
+      this.x1 = 0;      // Sets initial position of two images for each level of foreground
+      this.x11 = WIDTH;
+      this.x111 = WIDTH*2;
+      this.x1111 = WIDTH*3
       
-      this.x2 = WIDTH;
-      this.x22 = 0;
+      this.x2 = 0;
+      this.x22 = WIDTH;
+      this.x222 = WIDTH*2
+      this.x2222 = WIDTH*3    
 
-      this.x3 = WIDTH;
-      this.x33 = 0;
+      this.x3 = 0;
+      this.x33 = WIDTH;
+      this.x333 = WIDTH*2;
+      this.x3333 = WIDTH*3
     }
   
     setup() {
@@ -29,6 +35,10 @@ class Background {
         // image(this.bgImageLayer1, this.x11, 0, WIDTH, HEIGHT)
         // image(this.bgImageLayer1, this.x1, 0, WIDTH, HEIGHT)
 
+        // use for loop to count to 4, 
+        // for each iteration, add 
+       
+        
         fill(color('red'))
         rect(this.x33, HEIGHT - 400, WIDTH, 400)
         rect(this.x3, HEIGHT - 400, WIDTH, 400)
@@ -40,23 +50,32 @@ class Background {
         fill(color('blue'))
         rect(this.x11, HEIGHT - 200, WIDTH, 200)
         rect(this.x1, HEIGHT - 200, WIDTH, 200)
+
+        fill(color('orange'))
+        rect(this.x111, HEIGHT - 200, WIDTH, 200)
+        rect(this.x222, HEIGHT - 300, WIDTH, 300)
+
+        fill(color('purple'))
+        rect(this.x1111, HEIGHT - 200, WIDTH, 200)
+        rect(this.x2222, HEIGHT - 300, WIDTH, 300)
+
         
         // Loops through images once one has fully passed by screen
-        if (this.x1 <= -WIDTH) {
-            this.x1 = WIDTH
-        }
+        // if (this.x1 <= -WIDTH) {
+        //     this.x1 = WIDTH
+        // }
 
-        if (this.x11 <= -WIDTH) {
-            this.x11 = WIDTH;
-        }
+        // if (this.x11 <= -WIDTH) {
+        //     this.x11 = WIDTH;
+        // }
 
-        if (this.x2 <= -WIDTH) {
-            this.x2 = WIDTH;
-        }
+        // if (this.x2 <= -WIDTH) {
+        //     this.x2 = WIDTH;
+        // }
 
-        if (this.x22 <= -WIDTH) {
-            this.x22 = WIDTH;
-        }
+        // if (this.x22 <= -WIDTH) {
+        //     this.x22 = WIDTH;
+        // }
 
         if (this.x3 <= -WIDTH) {
             this.x3 = WIDTH;
@@ -67,19 +86,27 @@ class Background {
         }
 
         if (keyIsDown(RIGHT_ARROW)) {
-            this.x1 -= 3.5;
-            this.x11 -= 3.5;
-            this.x2 -= 3;
-            this.x22 -= 3;
+            this.x1 -= 4.5;
+            this.x11 -= 4.5;
+            this.x111 -= 4.5;
+            this.x1111 -= 4.5;
+            this.x2 -= 3.5;
+            this.x22 -= 3.5;
+            this.x222 -= 3.5;
+            this.x2222 -= 3.5;
             this.x3 -= 2.5;
             this.x33 -= 2.5;
         }
 
         if (keyIsDown(LEFT_ARROW)) {
-            this.x1 += 3.5;
-            this.x11 += 3.5;
-            this.x2 += 3;
-            this.x22 += 3;
+            this.x1 += 4.5;
+            this.x11 += 4.5;
+            this.x111 += 4.5
+            this.x1111 += 4.5
+            this.x2 += 3.5;
+            this.x22 += 3.5;
+            this.x222 += 3.5
+            this.x2222 += 3.5
             this.x3 += 2.5;
             this.x33 += 2.5;
         }
